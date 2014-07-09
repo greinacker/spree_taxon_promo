9562e7669d6c698f71c9cc258b315b4166c601be
Alexander Sysoev <alexander@acid.cl>
2014-07-09 17:44:01 -0400

    Fix calculator registtrator

--- lib/spree_taxon_promo/engine.rb
+++ lib/spree_taxon_promo/engine.rb
@@ -14,7 +14,7 @@ module SpreeTaxonPromo
     end
 
     initializer 'spree.promo.register.taxon_calculators', :after => "spree.promo.register.promotion.calculators"  do |app|
-      app.config.spree.calculators.promotion_actions_create_adjustments += [Spree::Calculator::FlatPercentTaxonTotal]
+      app.config.spree.calculators.promotion_actions_create_adjustments << Spree::Calculator::FlatPercentTaxonTotal
     end
 
     def self.activate
